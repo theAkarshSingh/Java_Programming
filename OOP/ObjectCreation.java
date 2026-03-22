0a3699d1d79348cb6ebe11b8712ebbc0328e59d6
Akarsh Singh <theakarshsingh@gmail.com>
2026-03-22 23:32:25 +0530

    Initial commit

---
+++ OOP/ObjectCreation.java
@@ -0,0 +1,15 @@
+package OOP;
+
+// Object is an entity that holds data(attributes) and can perform actions
+// Object is a reference data type
+public class ObjectCreation {
+    static void main() {
+        Car car = new Car();
+        car.start();
+        car.drive();
+        System.out.println(car.isRunning);
+        car.stop();
+        System.out.println(car.isRunning);
+
+    }
+}
