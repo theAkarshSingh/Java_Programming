7017a7dc0dd8b4657c995a86fe6bb34c3493646a
Akarsh Singh <theakarshsingh@gmail.com>
2026-04-01 00:30:32 +0530

    Initial commit

---
+++ Abstraction/Shape.java
@@ -0,0 +1,12 @@
+package Abstraction;
+
+//abstract -> used to define abstract classes & methods.
+//abstraction is the process of hiding implementation data and showing only the essential features
+
+public abstract class Shape {
+    abstract double area();
+
+    void display() { //Concrete method
+        System.out.println("This is a Shape.");
+    }
+}
